7e43391981685f13676c95485692a3fbe7a9690c
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2024-10-22 10:14:16 +0200

    add o1 and 4o mini

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -50,6 +50,12 @@ export interface Folder {
 }
 
 export type ModelOptions =
+  | 'o1-preview'
+  | 'o1-preview-2024-09-12'
+  | 'o1-mini'
+  | 'o1-mini-2024-09-12'
+  | 'gpt-4o-mini'
+  | 'gpt-4o-mini-2024-07-18'
   | 'gpt-4o'
   | 'gpt-4o-2024-05-13'
   | 'gpt-4'
